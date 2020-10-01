d88039d41ea2e14e5698a98ffbef8a8273edf97e
Jacquelineomollo <Jacquelineomollo@gmail.com>
2020-09-30 21:55:23 -0400

    initial setup

---
+++ store_app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import {Container, Row, Col} from "react-bootstrap";
+import './App.css';
+import Header from "./componnets/Header";
+import Footer from "./componnets/Footer";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+      <h1>Welcome To The Store of Your Dreams</h1>
+      </main>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
