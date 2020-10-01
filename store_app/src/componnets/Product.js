76d903bc848426d2c34f8e6315530a489ce46c05
Jacquelineomollo <Jacquelineomollo@gmail.com>
2020-10-01 10:00:36 -0400

    created rating page

--- store_app/src/componnets/Product.js
+++ store_app/src/componnets/Product.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { Card, CardImg } from 'react-bootstrap';
+import Rating from "./Rating";
 
 const Product = ({product}) => {
     return (
         
         <Card className="my-3 p-3 reound">
             <a href={`/product/${product._id}`}>
-                <CardImg src={product.image} variant="top" />
+            <CardImg src={product.image} variant="top" />
             </a>
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                <Card.Title>
+                    <strong>{product.name}</strong>
+                </Card.Title>
+            </a> 
+            <Card.Text as="div">
+                <Rating 
+                value={product.rating}
+                text={`${product.numReviews} reviews`}/>
+            </Card.Text>
+             <Card.Text as="h3">${product.price}</Card.Text>
+            </Card.Body>
         </Card>
       
     )
